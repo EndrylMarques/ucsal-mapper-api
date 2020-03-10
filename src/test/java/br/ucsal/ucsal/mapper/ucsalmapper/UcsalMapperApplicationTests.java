8ab56d38334eb1572fa91ddeddf71a595b959690
Endryl Marques <endryl.santos@solutis.com.br>
2020-03-10 12:55:06 -0300

    First Commit

---
+++ src/test/java/br/ucsal/ucsal/mapper/ucsalmapper/UcsalMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.ucsal.mapper.ucsalmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcsalMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
